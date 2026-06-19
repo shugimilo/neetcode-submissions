26b86dc2a368e049091107922ae3c9cabfd5b5f3
Petar Milojević <151462860+shugimilo@users.noreply.github.com>
2026-06-19 22:24:06 +0200

    Add: reverse-integer - submission-1

---
+++ Data Structures & Algorithms/reverse-integer/submission-1.py
@@ -0,0 +1,30 @@
+def isPositiveInt(x):
+    return x > 0
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        isPositive = isPositiveInt(x)
+
+        reverseX = 0
+
+        x = abs(x)
+
+        while x >= 10:
+            whole = x // 10
+            modulo = x % 10
+
+            reverseX += modulo
+            reverseX *= 10
+
+            x = whole
+
+        reverseX += x
+
+        if reverseX >= (2**31) - 1 or -reverseX <= -(2**31):
+            return 0
+
+        if isPositive:
+            return reverseX
+        else:
+            return -reverseX
+        
