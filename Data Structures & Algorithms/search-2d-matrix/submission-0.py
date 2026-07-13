1416709e50b6e75f345c7ccb21b3bcac4766ed2e
Petar Milojević <151462860+shugimilo@users.noreply.github.com>
2026-07-13 12:08:28 +0200

    Add: search-2d-matrix - submission-0

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-0.py
@@ -0,0 +1,29 @@
+class Solution:
+    def convertToCoords(self, position: int, n: int) -> tuple(int, int):
+        row = position // n
+        col = position % n
+
+        return (row, col)
+
+    def convertToPosition(self, row: int, col: int, m: int, n: int) -> int:
+        return m*row + n*col
+
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m = len(matrix)
+        n = len(matrix[0])
+
+        left = 0
+        right = m*n-1
+
+        while left <= right:
+            mid = (left + right) // 2
+            midRow, midCol = self.convertToCoords(mid, n)
+            if matrix[midRow][midCol] < target:
+                left = mid + 1
+            elif matrix[midRow][midCol] > target:
+                right = mid - 1
+            else:
+                return True
+
+        return False
+
