3e52f6fcdde71872e98d0fde0cf67266d0f36768
Petar Milojević <151462860+shugimilo@users.noreply.github.com>
2026-07-17 15:02:57 +0200

    Add: non-cyclical-number - submission-7

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-7.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findNext(self, n: int) -> int:
+        newN = 0
+        while n >= 10:
+            newN += (n%10)**2
+            n = n//10
+        newN += n**2
+        return newN
+
+    def isHappy(self, n: int) -> bool:
+        if n == 1:
+            return True
+        m = self.findNext(self.findNext(n))
+        while n != m:
+            n = self.findNext(n)
+            m = self.findNext(self.findNext(m))
+            if n == 1 or m == 1:
+                return True
+        return False
