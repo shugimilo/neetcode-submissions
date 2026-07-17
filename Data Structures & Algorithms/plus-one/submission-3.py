1618200ec6646b9b43f2305f40d5574e502048a6
Petar Milojević <151462860+shugimilo@users.noreply.github.com>
2026-07-18 01:24:47 +0200

    Add: plus-one - submission-3

---
+++ Data Structures & Algorithms/plus-one/submission-3.py
@@ -0,0 +1,23 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        carry = 1
+        res = []
+        while digits and carry:
+            digit = digits.pop()
+            digit = digit + carry
+            if digit != 10:
+                carry = 0
+            else:
+                digit = 0
+            res.append(digit)
+
+        if digits:
+            digits.reverse()
+            res += digits
+        if carry:
+            res.append(1)
+
+        res.reverse()
+        return res
+
+
