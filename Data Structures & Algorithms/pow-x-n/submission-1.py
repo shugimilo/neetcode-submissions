65c791052547a4e5c9dc127857f898c87a466040
Petar Milojević <151462860+shugimilo@users.noreply.github.com>
2026-07-18 15:10:43 +0200

    Add: pow-x-n - submission-1

---
+++ Data Structures & Algorithms/pow-x-n/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n == 0:
+            return float(1)
+        positive = False if n < 0 else True
+        n = abs(n)
+
+        print(f"x: {x}, n: {n}")
+
+        i=0
+        res=x
+        while n > 1:
+            res*=x
+            n -= 1
+            print(f"\titeration: {i}, updated res to {res} and n to {n}")
+            i+=1
+
+        if not positive:
+            res = 1/res
+        return res
